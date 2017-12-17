de4cefe1b713a35b18bc48028739e9b0fc954495
SchumyHao <bob-hjl@126.com>
2017-12-17 20:39:46 +0800

    update version
    Signed-off-by: SchumyHao <bob-hjl@126.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='hassmart-home-assistant-frontend',
-      version='20171206.0',
+      version='20171217.0',
       description='The Hassmart frontend, powered by homeassistant',
       url='https://github.com/hassmart/home-assistant-polymer',
       author='The Home Assistant Authors',
